922bc44cd6e0869a0def85b4ed55d7aa39d2460d
Angel <angelj@edivers.mx>
2020-01-27 18:04:33 -0600

    Updates

---
+++ precache-manifest.ce910eb55a5955900cb75026cceb77d2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9086f9175b2d809306b558d76483d2c8",
+    "url": "/ccd-demo/index.html"
+  },
+  {
+    "revision": "09a925bb1fafb53b19c5",
+    "url": "/ccd-demo/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "47f5a721bc4ad1924364",
+    "url": "/ccd-demo/static/css/main.714a2218.chunk.css"
+  },
+  {
+    "revision": "09a925bb1fafb53b19c5",
+    "url": "/ccd-demo/static/js/2.7f1d9ccd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ccd-demo/static/js/2.7f1d9ccd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "47f5a721bc4ad1924364",
+    "url": "/ccd-demo/static/js/main.7e5934cf.chunk.js"
+  },
+  {
+    "revision": "b95f9b81240225b569b1",
+    "url": "/ccd-demo/static/js/runtime-main.542e118e.js"
+  },
+  {
+    "revision": "727975f24f9579d43ad0d9b936bd5aa8",
+    "url": "/ccd-demo/static/media/Web.727975f2.png"
+  },
+  {
+    "revision": "1e3741f3dc5303c2e11f87cf6f12bcdf",
+    "url": "/ccd-demo/static/media/diseno.1e3741f3.jpg"
+  },
+  {
+    "revision": "3d90c604aa506ac3883cb28364696086",
+    "url": "/ccd-demo/static/media/ux.3d90c604.png"
+  }
+]);
